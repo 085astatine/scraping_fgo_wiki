3ce98ff87694226c4990a05526c5688eb65a9b87
085astatine <astatine085@gmail.com>
2020-03-19 16:50:35 +0900

    Use lxml & requests instead of selenium

--- lib/servant.py
+++ lib/servant.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from typing import List, TypedDict
-import selenium.webdriver
+import lxml.html
+import requests
 
 
 _Servant = TypedDict(
@@ -15,8 +16,7 @@ _Servant = TypedDict(
         total=False)
 
 
-def _parse_servant_table(
-        driver: selenium.webdriver.remote.webdriver.WebDriver):
+def _parse_servant_table():
     # URL: サーヴァント > 一覧 > 番号順
     url = 'https://w.atwiki.jp/f_go/pages/713.html'
     # 入手不可サーヴァントID
@@ -44,22 +44,20 @@ def _parse_servant_table(
             '降': 'Foreigner',  # フォーリナー
             '獣': 'Beast'}  # ビースト
     # access
-    driver.get(url)
+    response = requests.get(url)
+    etree = lxml.html.fromstring(response.text)
     xpath = ('/html/body//div[@id="wikibody"]/div[2]/div/'
              'table/tbody/tr[td]')
     # サーヴァントリスト作成
     result: List[_Servant] = []
-    for row in driver.find_elements_by_xpath(xpath):
-        servant_id = int(row.find_elements_by_xpath('td[1]')[0].text)
+    for row in etree.xpath(xpath):
+        servant_id = int(row.xpath('td[1]')[0].text)
         if servant_id in ignore_servant_ids:
             continue
-        rarity = int(row.find_elements_by_xpath('td[2]')[0].text)
-        servant_name = row.find_elements_by_xpath('td[3]')[0].text
-        servant_class = to_servant_class[
-                row.find_elements_by_xpath('td[4]')[0].text]
-        servant_url = (
-                row.find_elements_by_xpath('td[3]//a')[0]
-                .get_attribute('href'))
+        rarity = int(row.xpath('td[2]')[0].text)
+        servant_name = row.xpath('td[3]//a')[0].text
+        servant_class = to_servant_class[row.xpath('td[4]')[0].text.strip()]
+        servant_url = row.xpath('td[3]//a')[0].get('href')
         result.append({
                 'id': servant_id,
                 'class': servant_class,
@@ -69,7 +67,6 @@ def _parse_servant_table(
     return result
 
 
-def servant_list(
-        driver: selenium.webdriver.remote.webdriver.WebDriver):
-    servant_list = _parse_servant_table(driver)
+def servant_list():
+    servant_list = _parse_servant_table()
     print(servant_list)
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import selenium.webdriver
 import lib.servant
 
 
 def main():
-    options = selenium.webdriver.ChromeOptions()
-    options.add_argument('--headless')
-
-    driver = selenium.webdriver.Chrome(options=options)
-
-    lib.servant.servant_list(driver)
-
-    driver.close()
+    lib.servant.servant_list()
 
 
 if __name__ == '__main__':
