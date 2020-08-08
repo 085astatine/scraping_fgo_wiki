676cebd25ccf8bd250a8ebaaa698c7f7696821de
085astatine <astatine085@gmail.com>
2020-08-09 02:23:42 +0900

    Parse the table of Craft Essences obtained by bond level

--- craft_essence.py
+++ craft_essence.py
@@ -4,6 +4,7 @@
 
 import logging
 import re
+import time
 from typing import NamedTuple, List, Optional
 import lxml.html
 import requests
@@ -17,9 +18,8 @@ class CraftEssence(NamedTuple):
 
 def parse_row(
         row: lxml.html.HtmlElement,
-        logger: Optional[logging.Logger] = None) -> List[CraftEssence]:
+        logger: Optional[logging.Logger] = None) -> Optional[CraftEssence]:
     logger = logger or logging.getLogger(__name__)
-    result: List[CraftEssence] = []
     # id, rarity
     id_cell = row.xpath('td[1]')[0].text_content().strip()
     rarity_cell = row.xpath('td[2]')[0].text_content().strip()
@@ -33,30 +33,71 @@ def parse_row(
     elif id_cell.isdigit():
         logger.debug('id: %d', int(id_cell))
         name = row.xpath('td[3]')[0].xpath('a')[0].text.strip()
-        logger.info(
-                'id=%d, rarity=%d, name="%s"',
-                int(id_cell),
-                int(rarity_cell),
-                name)
-        result.append(CraftEssence(
+        result = CraftEssence(
                 id=int(id_cell),
                 rarity=int(rarity_cell),
-                name=name))
-    else:
-        logger.debug('this row is not target')
+                name=name)
+        logger.debug('parse row: %s', result)
+        return result
+    logger.debug('this row is not target')
+    return None
+
+
+def parse_bond_craft_essences(
+        logger: Optional[logging.Logger]) -> List[CraftEssence]:
+    logger = logger or logging.getLogger(__name__)
+    result: List[CraftEssence] = []
+    url = 'https://w.atwiki.jp/f_go/pages/1106.html'
+    response = requests.get(url)
+    root = lxml.html.fromstring(response.text)
+    xpath = '//*[@id="wikibody"]/div[3]/div/div/table/tbody/tr[td]'
+    for row in root.xpath(xpath):
+        cells = row.xpath('td')
+        if not cells[4].text.strip().isdigit():
+            continue
+        # servant
+        servant_id = int(cells[0].text)
+        servant_name = cells[2].xpath('a')[0].text
+        logger.debug('servant: No.%03d %s', servant_id, servant_name)
+        # craft essence
+        craft_essence_id = int(cells[4].text)
+        craft_essence_rarity = 4
+        craft_essence_name = cells[5].xpath('a')[0].text
+        logger.info(
+                'id=%d, rarity=%d, name="%s"',
+                craft_essence_id,
+                craft_essence_rarity,
+                craft_essence_name)
+        result.append(CraftEssence(
+                id=craft_essence_id,
+                rarity=craft_essence_rarity,
+                name=craft_essence_name))
+        logger.info('bond: %s', result[-1])
     return result
 
 
 def main(logger: Optional[logging.Logger] = None):
     logger = logger or logging.getLogger(__name__)
-    # craft essence list
+    sleep = 1.0
     craft_essences: List[CraftEssence] = []
+    # bond craft essences
+    craft_essences.extend(parse_bond_craft_essences(logger=logger))
+    time.sleep(sleep)
+    # normal craft essence
     url = 'https://w.atwiki.jp/f_go/pages/32.html'
     response = requests.get(url)
     root = lxml.html.fromstring(response.text)
     xpath = '//*[@id="wikibody"]/div[3]/div/div/table/tbody/tr[td]'
     for row in root.xpath(xpath):
-        craft_essences.extend(parse_row(row, logger))
+        craft_essence = parse_row(row, logger)
+        if craft_essence is not None:
+            if not any(x.id == craft_essence.id for x in craft_essences):
+                craft_essences.append(craft_essence)
+                logger.info('normal: %s', craft_essence)
+            else:
+                logger.debug('duplicated: %s', craft_essence)
+    # sort by id
+    craft_essences.sort(key=lambda x: x.id)
     # write as csv
     with open('craft_essences.csv', mode='w') as csv:
         for craft_essence in craft_essences:
