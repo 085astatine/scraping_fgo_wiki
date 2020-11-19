a895399192d1433ac08fc8da8d1a4b47f40e0755
085astatine <astatine085@gmail.com>
2020-11-19 20:17:41 +0900

    Add save_json()

--- main.py
+++ main.py
@@ -5,27 +5,33 @@ import argparse
 import json
 import logging
 import pathlib
-from typing import List
+from typing import Any, List
 import lib
+
+
+def save_json(
+        path: pathlib.Path,
+        data: Any) -> None:
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+    with path.open(mode='w') as file:
+        json.dump(
+                data,
+                file,
+                ensure_ascii=False,
+                indent=2)
 
 
 def load_items(
         path: pathlib.Path,
         *,
         force_update: bool = False) -> List[lib.Item]:
-    if not path.parent.exists():
-        path.parent.mkdir(parents=True)
     if path.exists() and not force_update:
         with path.open() as item_file:
             items = json.load(item_file)
     else:
         items = lib.item_list()
-        with path.open(mode='w') as item_file:
-            json.dump(
-                    items,
-                    item_file,
-                    ensure_ascii=False,
-                    indent=2)
+        save_json(path, items)
     return items
 
 
@@ -67,32 +73,18 @@ def main():
                 force_update=option.force or option.mode == 'item')
     # servant
     servant_path = pathlib.Path('data/servants.json')
-    if not servant_path.parent.exists():
-        servant_path.parent.mkdir(parents=True)
     if servant_path.exists() and not force_update:
         with servant_path.open() as servant_file:
             servants = json.load(servant_file)
     else:
         servants = lib.servant.servant_list(items)
-        with servant_path.open(mode='w') as servant_file:
-            json.dump(
-                    servants,
-                    servant_file,
-                    ensure_ascii=False,
-                    indent=2)
+        save_json(servant_path, servants)
     # master data
     master_data_path = pathlib.Path('data/master_data.json')
-    if not master_data_path.parent.exists():
-        master_data_path.parent.mkdir(parents=True)
     master_data = {
             'items': items,
             'servants': servants}
-    with master_data_path.open(mode='w') as master_data_file:
-        json.dump(
-                master_data,
-                master_data_file,
-                ensure_ascii=False,
-                indent=2)
+    save_json(master_data_path, master_data)
 
 
 if __name__ == '__main__':
