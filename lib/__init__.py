f9f0230515372d62c447a457334d2c6232d4b22a
085astatine <astatine085@gmail.com>
2020-11-20 16:35:29 +0900

    Define Dictionary

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,6 +3,7 @@
 import logging
 from .item import Item, item_list
 from .servant import servant_list
+from .text import Dictionary, Text
 
 
 logging.getLogger(__package__).addHandler(logging.NullHandler())
--- lib/text.py
+++ lib/text.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from typing import TypedDict
+from typing import Dict, TypedDict
 
 
 class Text(TypedDict):
     jp: str
     en: str
+
+
+class Dictionary(TypedDict):
+    item: Dict[str, Text]
+    servant: Dict[str, Text]
+    skill: Dict[str, Text]
