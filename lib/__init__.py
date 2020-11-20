a98a4fb320c2cefb53d76299de1c7ff54d76debd
085astatine <astatine085@gmail.com>
2020-11-20 16:44:53 +0900

    Add item_dict()

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from .item import Item, item_list
+from .item import Item, item_dict, item_list
 from .servant import servant_list
 from .text import Dictionary, Text
 
--- lib/item.py
+++ lib/item.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from typing import List, TypedDict
+from typing import Dict, List, TypedDict
 import lxml.html
 import requests
 from .text import Text
@@ -56,4 +56,25 @@ def item_list() -> List[Item]:
                 'name': {
                     'jp': name_jp,
                     'en': name_en}})
+    return result
+
+
+def item_dict() -> Dict[str, Text]:
+    url = 'https://grandorder.wiki/Items'
+    response = requests.get(url)
+    root = lxml.html.fromstring(response.text)
+    xpath = (
+            '//h2[span[normalize-space()="Item List"]]'
+            '/following-sibling::table//tr[td]')
+    target_item_types = ['Ascension', 'Skill']
+    result: Dict[str, Text] = {}
+    for row in root.xpath(xpath):
+        item_type = row.xpath('td[4]')[0].text.strip()
+        if not any(map(lambda x: x in item_type, target_item_types)):
+            continue
+        name_cell = row.xpath('td[3]')[0]
+        name_jp = name_cell.xpath('br/following-sibling::text()')[0].strip()
+        name_en = name_cell.xpath('a')[0].text.strip()
+        _logger.debug('jp: %s, en: %s', name_jp, name_en)
+        result[name_jp] = {'jp': name_jp, 'en': name_en}
     return result
