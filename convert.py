833025c19b38e8843330b3d12521a8465a95c990
085astatine <astatine085@gmail.com>
2021-04-27 20:26:56 +0900

    Add option to convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import logging
 import pathlib
 import yaml
@@ -12,6 +13,22 @@ def main() -> None:
     logger = logging.getLogger('to_yaml')
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
+    # option
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument(
+                '--to',
+                dest='to',
+                choices=['json', 'yaml'],
+                help='output format')
+    argument_parser.add_argument(
+                '-v', '--verbose',
+                dest='verbose',
+                action='store_true',
+                help='set log level to debug')
+    option = argument_parser.parse_args()
+    if option.verbose:
+        logger.setLevel(logging.DEBUG)
+    logger.debug('option: %s', option)
     # directory
     root_directory = pathlib.Path(__file__).resolve().parent
     json_directory = root_directory.joinpath('data')
@@ -20,8 +37,10 @@ def main() -> None:
     logger.info('json directory: %s', json_directory)
     logger.info('yaml directory: %s', yaml_directory)
     # convert
-    to_yaml(json_directory, yaml_directory, logger)
-    to_json(yaml_directory, json_directory, logger)
+    if option.to == 'json':
+        to_json(yaml_directory, json_directory, logger)
+    if option.to == 'yaml':
+        to_yaml(json_directory, yaml_directory, logger)
 
 
 def to_yaml(
