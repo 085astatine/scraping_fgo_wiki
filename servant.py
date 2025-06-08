4bc7e26c65c5bf469e3be76e033260e29d320f99
085astatine <astatine085@gmail.com>
2025-06-08 19:11:39 +0900

    Add servant.py

---
+++ servant.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from __future__ import annotations
+
+import argparse
+import dataclasses
+import logging
+
+
+def main() -> None:
+    # logger
+    logger = create_logger()
+    logger.info("servant")
+    # option
+    option = Option(**vars(argument_parser().parse_args()))
+    if option.verbose:
+        logger.setLevel(logging.DEBUG)
+    logger.debug("option: %s", option)
+
+
+def create_logger() -> logging.Logger:
+    logger = logging.getLogger("servant")
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.formatter = logging.Formatter(
+        fmt="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+    )
+    logger.addHandler(handler)
+    return logger
+
+
+@dataclasses.dataclass(frozen=True)
+class Option:
+    verbose: bool
+    force_update: bool
+    request_interval: float
+
+
+def argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Generate English servant dictionary",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="set log level to debug",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        dest="force_update",
+        action="store_true",
+        help="force update",
+    )
+    parser.add_argument(
+        "--request-interval",
+        dest="request_interval",
+        type=float,
+        default=5.0,
+        help="request interval seconds (default: %(default)s)",
+        metavar="SECONDS",
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    main()
